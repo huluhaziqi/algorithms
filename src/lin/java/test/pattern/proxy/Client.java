88b85a82628c9192d19b4c81a88d25a99228b8d3
huluhaziqi <huluhaziqi@gmail.com>
2018-06-11 17:18:32 +0800

    agg proxy

---
+++ src/lin/java/test/pattern/proxy/Client.java
@@ -0,0 +1,20 @@
+package lin.java.test.pattern.proxy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Client {
+    public static void main(String[] args) {
+        //使用虚拟的代理，模拟图片延时加载过程，用临时图片片去替换，指导图片加载完毕
+        String image = "http://image.jpg";
+        try {
+            URL url = new URL(image);
+            HighResolutionImage highResolutionImage = new HighResolutionImage(url);
+            Proxy proxy = new Proxy(highResolutionImage);
+            proxy.showImage();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/lin/java/test/pattern/proxy/Image.java
@@ -0,0 +1,6 @@
+package lin.java.test.pattern.proxy;
+
+public interface Image {
+
+    void showImage();
+}
