1e5160fecc558ba7e28d602e5d85ef2576100f16
huluhaziqi <huluhaziqi@gmail.com>
2018-11-04 19:04:00 +0800

    add io/selector

---
+++ src/lin/java/test/io/WebClient.java
@@ -0,0 +1,25 @@
+package lin.java.test.io;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class WebClient {
+    public static void main(String[] args) throws Exception{
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("127.0.0.1", 8000));
+        ByteBuffer byteBuffer = ByteBuffer.allocate(128);
+        byteBuffer.put("hello service this is client".getBytes());
+        byteBuffer.flip();
+        ByteBuffer readBuffer = ByteBuffer.allocate(128);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true) {
+            byteBuffer.rewind();
+            socketChannel.write(byteBuffer);
+            readBuffer.clear();
+            socketChannel.read(readBuffer);
+            stringBuilder.append((char) readBuffer.get());
+            System.out.println(stringBuilder.toString());
+        }
+    }
+}
