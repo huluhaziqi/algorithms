f944a02973e34d3366b3b16ae4798a37d74aa761
huluhaziqi <huluhaziqi@gmail.com>
2019-11-29 14:48:29 +0800

    add bucket sort

---
+++ src/com/huluhaziqi/algorithms/algorithms/sort/LeetCode451.java
@@ -0,0 +1,43 @@
+package com.huluhaziqi.algorithms.algorithms.sort;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode451 {
+
+    class Solution {
+        public String frequencySort(String s) {
+            if (s == null || s.length() == 0) {
+                return "";
+            }
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                if (!map.containsKey(c)) {
+                    map.put(c, 1);
+                } else {
+                    map.put(c, map.get(c) + 1);
+                }
+            }
+            List<List<Character>> freq = new ArrayList<>();
+            for (int i = 0; i <= s.length(); i++) {
+                freq.add(new ArrayList<>());
+            }
+            for (Character c : map.keySet()) {
+                int value = map.get(c);
+                freq.get(value).add(c);
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = freq.size() - 1; i >= 0; i--) {
+                List<Character> sub = freq.get(i);
+                for (Character c : sub) {
+                    for (int k = 0; k < i; k++) {
+                        sb.append(c);
+                    }
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
