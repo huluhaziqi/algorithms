84bb56a2d864b1fce4fb5288da71e53ab9339708
huluhaziqi <huluhaziqi@gmail.com>
2019-12-07 22:56:00 +0800

    add leetcode 69

--- src/com/huluhaziqi/algorithms/algorithms/binary_search/Sqrt.java
+++ src/com/huluhaziqi/algorithms/algorithms/binary_search/Sqrt.java
@@ -26,4 +26,25 @@ public class Sqrt {
         }
         return (int) r;
     }
+
+    class Solution {
+        public int mySqrt(int x) {
+            if(x <= 0){
+                return 0;
+            }
+            int l = 1;
+            int r = x / 2 + 1;
+            while(l <= r){
+                int mid = (r - l) / 2 + l;
+                if(mid == x / mid){
+                    return mid;
+                } else if(mid > x / mid){
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return r;
+        }
+    }
 }
