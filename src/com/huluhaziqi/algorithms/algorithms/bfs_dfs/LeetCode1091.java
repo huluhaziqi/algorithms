3ad88b3cb891337ffa787185dbff44314ff36ca5
huluhaziqi <huluhaziqi@gmail.com>
2019-12-30 00:50:45 +0800

    add leetcode 1091 bfs

---
+++ src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/LeetCode1091.java
@@ -0,0 +1,58 @@
+package com.huluhaziqi.algorithms.algorithms.bfs_dfs;
+
+import javafx.util.Pair;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeetCode1091 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] grid = {{0}};
+        solution.shortestPathBinaryMatrix(grid);
+    }
+
+    static class Solution {
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            if (grid == null || grid.length == 0 || grid[0].length == 0 || grid[0][0] == 1) {
+                return -1;
+            }
+
+            int[][] directions = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+            int len = grid.length;
+            int rowLen = grid[0].length;
+            boolean[][] visited = new boolean[len][rowLen];
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+            visited[0][0] = true;
+            int result = 1;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+
+                while (size-- > 0) {
+                    int[] top = queue.poll();
+                    if (top[0] == len - 1 && top[1] == len - 1) {
+                        return result;
+                    }
+                    int row = top[0];
+                    int col = top[1];
+                    grid[row][col] = 1;
+                    for (int i = 0; i < 8; i++) {
+                        int rowT = row + directions[i][0];
+                        int colT = col + directions[i][1];
+                        if (rowT >= 0 && rowT < len && colT >= 0 && colT < rowLen && !visited[rowT][colT] && grid[rowT][colT] == 0) {
+                            visited[rowT][colT] = true;
+                            queue.offer(new int[]{rowT, colT});
+                        }
+
+                    }
+                }
+                result++;
+            }
+            return -1;
+        }
+
+    }
+}
--- src/com/huluhaziqi/algorithms/algorithms/binary_search/LeetCode540.java
+++ src/com/huluhaziqi/algorithms/algorithms/binary_search/LeetCode540.java
@@ -2,25 +2,40 @@ package com.huluhaziqi.algorithms.algorithms.binary_search;
 
 public class LeetCode540 {
 
+
+    /**
+     * 给定一个只包含整数的有序数组，每个元素都会出现两次，唯有一个数只会出现一次，找出这个数。
+     *
+     * 示例 1:
+     *
+     * 输入: [1,1,2,3,3,4,4,8,8]
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: [3,3,7,7,10,11,11]
+     * 输出: 10
+     * 注意: 您的方案应该在 O(log n)时间复杂度和 O(1)空间复杂度中运行。
+     *
+     */
     class Solution {
         public int singleNonDuplicate(int[] nums) {
-            if(nums == null || nums.length == 0){
+            if (nums == null || nums.length == 0) {
                 return -1;
             }
             int l = 0;
             int r = nums.length - 1;
-            while(l < r){
+            while (l < r) {
                 int m = l + (r - l) / 2;
-                if((m % 2) == 1){
-                    if(nums[m] == nums[m + 1]) {
+                if ((m % 2) == 1) {
+                    if (nums[m] == nums[m + 1]) {
                         r = m - 1;
                     } else {
                         l = m + 1;
                     }
                 } else {
-                    if(nums[m] == nums[m + 1]){
+                    if (nums[m] == nums[m + 1]) {
                         l = m + 2;
-                    } else{
+                    } else {
                         r = m;
                     }
                 }
@@ -31,17 +46,17 @@ public class LeetCode540 {
 
     class Solution2 {
         public int singleNonDuplicate(int[] nums) {
-            if(nums == null || nums.length == 0){
+            if (nums == null || nums.length == 0) {
                 return -1;
             }
             int l = 0;
             int r = nums.length - 1;
-            while(l < r){
+            while (l < r) {
                 int m = l + (r - l) / 2;
-                if((m % 2) == 1){
-                   m--;
+                if ((m % 2) == 1) {
+                    m--;
                 }
-                if(nums[m] == nums[m + 1]){
+                if (nums[m] == nums[m + 1]) {
                     l = m + 2;
                 } else {
                     r = m;
