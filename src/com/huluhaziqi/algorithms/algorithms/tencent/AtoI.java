8a9a36915893938c645d3e6b98fd05b9a69ac9ec
huluhaziqi <huluhaziqi@gmail.com>
2020-02-22 23:19:06 +0800

    [add]leetcode 8

--- src/com/huluhaziqi/algorithms/algorithms/tencent/AtoI.java
+++ src/com/huluhaziqi/algorithms/algorithms/tencent/AtoI.java
@@ -5,43 +5,39 @@ public class AtoI {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String str = "   -42";
+        String str = "42";
         int result = solution.myAtoi(str);
         System.out.println(result);
     }
 
     static class Solution {
         public int myAtoi(String str) {
-            if (str == null || str.length() == 0) {
+            if (str == null || str.trim().length() == 0) {
                 return 0;
             }
             str = str.trim();
-            if (str.length() == 0) {
-                return 0;
-            }
-            char c = str.charAt(0);
-            if ((c >= '0' && c <= '9') || c == '-' || c == '+') {
-                String[] string = str.split(" ");
-                str = string[0];
-            }
-            boolean flag = false;
+            int tag = 1;
             long result = 0;
-            int index = 1;
+            int temp = 1;
             for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '-') {
-                    flag = true;
-                    continue;
-                }
-                result = result * 10 + (str.charAt(i) - '0');
-                boolean tem = ((result > Integer.MAX_VALUE) && !flag) ;
-                boolean tem2 = ((result > Integer.MAX_VALUE + 1) && flag);
-                System.out.println("tem " + tem + "tem2 " +tem2);
-
-                if (((result > Integer.MAX_VALUE) && !flag) || ((result > (long) (Integer.MAX_VALUE + 1)) && flag)) {
-                    return (flag ? Integer.MIN_VALUE : Integer.MAX_VALUE);
+                char c = str.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    int tem = c - '0';
+                    result = result * 10 + tem;
+                    if (tag == 1 && result >= Integer.MAX_VALUE) {
+                        return Integer.MAX_VALUE;
+                    }
+                    if (tag == -1 && result > Integer.MAX_VALUE) {
+                        return Integer.MIN_VALUE;
+                    }
+                } else if ((c == '-' || c == '+') && i == 0) {
+                    tag = (c == '-') ? -1 : 1;
+                } else {
+                    break;
                 }
             }
-            return (int) result * (flag ? -1 : 1);
+            return (int) (result * tag);
         }
+
     }
 }
