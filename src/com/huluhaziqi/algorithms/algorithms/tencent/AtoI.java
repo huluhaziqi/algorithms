5da494eda94de97195020b267233a21628363e81
huluhaziqi <huluhaziqi@gmail.com>
2020-02-22 23:35:36 +0800

    [add] use not time of atoI

--- src/com/huluhaziqi/algorithms/algorithms/tencent/AtoI.java
+++ src/com/huluhaziqi/algorithms/algorithms/tencent/AtoI.java
@@ -40,4 +40,45 @@ public class AtoI {
         }
 
     }
+
+    class Solution2 {
+        public int myAtoi(String str) {
+            if (str == null || str.trim().length() == 0) {
+                return 0;
+            }
+            int len = str.length();
+            int tag = 1;
+            long result = 0;
+            int start = 0;
+            while (start < len) {
+                if (str.charAt(start) == ' ') {
+                    start++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            if ((str.charAt(start) == '-' || str.charAt(start) == '+')) {
+                tag = (str.charAt(start) == '-') ? -1 : 1;
+                start++;
+            }
+            for (int i = start; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c >= '0' && c <= '9') {
+                    int tem = c - '0';
+                    result = result * 10 + tem;
+                    if (tag == 1 && result >= Integer.MAX_VALUE) {
+                        return Integer.MAX_VALUE;
+                    }
+                    if (tag == -1 && result > Integer.MAX_VALUE) {
+                        return Integer.MIN_VALUE;
+                    }
+                } else {
+                    break;
+                }
+            }
+            return (int) (result * tag);
+        }
+
+    }
 }
