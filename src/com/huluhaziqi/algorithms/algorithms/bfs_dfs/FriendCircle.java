f13faca8232bb7670540d3af4cf9659546b27aa9
huluhaziqi <huluhaziqi@gmail.com>
2020-02-24 00:13:31 +0800

    [add] leetcode 130

--- src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/FriendCircle.java
+++ src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/FriendCircle.java
@@ -3,7 +3,7 @@ package com.huluhaziqi.algorithms.algorithms.bfs_dfs;
 public class FriendCircle {
 
     public static void main(String[] args) {
-        int[][] grid = {{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
+        int[][] grid = {{1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1, 0, 1, 1}};
         int result = new FriendCircle().findCircleNum(grid);
         System.out.println(result);
     }
@@ -37,8 +37,8 @@ public class FriendCircle {
         int len = M.length;
         boolean[] visited = new boolean[len];
         int count = 0;
-        for(int i = 0; i < len ;i++){
-            if(visited[i] == false) {
+        for (int i = 0; i < len; i++) {
+            if (visited[i] == false) {
                 dfs(M, i, visited);
                 count++;
             }
@@ -47,12 +47,40 @@ public class FriendCircle {
 
     }
 
-    public void dfs(int grid[][] ,int k ,boolean[] visited){
-        for(int i = 0 ; i < grid.length;i++){
-            if(grid[k][i] == 1 && visited[i] == false){
+    public void dfs(int grid[][], int k, boolean[] visited) {
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[k][i] == 1 && visited[i] == false) {
                 visited[i] = true;
-                dfs(grid,i,visited);
+                dfs(grid, i, visited);
             }
+        }
+    }
+
+    class Solution {
+        public int findCircleNum(int[][] M) {
+            if (M == null || M.length == 0 || M[0] == null || M[0].length == 0) {
+                return 0;
+            }
+            int count = 0;
+            boolean[] visited = new boolean[M.length];
+            for (int i = 0; i < M.length; i++) {
+                if (!visited[i]) {
+                    count += ((dfs(M, i, visited) >= 1) ? 1 : 0);
+                }
+            }
+            return count;
+        }
+
+
+        int dfs(int[][] m, int i, boolean[] visited) {
+            int count = 1;
+            for (int k = 0; k < m.length; k++) {
+                if (!visited[k] && m[i][k] == 1) {
+                    visited[k] = true;
+                    count += dfs(m, k, visited);
+                }
+            }
+            return count;
         }
     }
 
--- src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/SurroundedRegions.java
+++ src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/SurroundedRegions.java
@@ -4,6 +4,41 @@ public class SurroundedRegions {
 
     int[][] direct = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
+    /**
+     * LeetCode130. Surrounded Regions
+     * Medium
+     * <p>
+     * 1179
+     * <p>
+     * 539
+     * <p>
+     * Add to List
+     * <p>
+     * Share
+     * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+     * <p>
+     * A region is captured by flipping all 'O's into 'X's in that surrounded region.
+     * <p>
+     * Example:
+     * <p>
+     * X X X X
+     * X O O X
+     * X X O X
+     * X O X X
+     * After running your function, the board should be:
+     * <p>
+     * X X X X
+     * X X X X
+     * X X X X
+     * X O X X
+     * Explanation:
+     * <p>
+     * Surrounded regions shouldn’t be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. Two cells are connected if they are adjacent cells connected horizontally or vertically.
+     * <p>
+     * Accepted
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         char[][] board = {{'X', 'O', 'X', 'O', 'X', 'O'},
                 {'O', 'X', 'O', 'X', 'O', 'X'},
@@ -11,8 +46,8 @@ public class SurroundedRegions {
                 'O', 'X', 'O', 'X', 'O', 'X'}
         };
         new SurroundedRegions().solve(board);
-        for(char[] c : board){
-            for(char t : c){
+        for (char[] c : board) {
+            for (char t : c) {
                 System.out.print(t + " ");
             }
             System.out.println();
@@ -55,38 +90,74 @@ public class SurroundedRegions {
     }
 
     public void solve(char[][] board) {
-        if(board == null || board.length == 0 || board[0].length == 0){
+        if (board == null || board.length == 0 || board[0].length == 0) {
             return;
         }
-        for(int i = 0; i < board[0].length; i++){
-            dfs(board,0,i);
-            dfs(board,board.length - 1,i);
+        for (int i = 0; i < board[0].length; i++) {
+            dfs(board, 0, i);
+            dfs(board, board.length - 1, i);
         }
-        for(int i = 0; i < board.length ;i++){
-            dfs(board,i,0);
-            dfs(board,i,board[0].length - 1);
+        for (int i = 0; i < board.length; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, board[0].length - 1);
         }
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length ;j++){
-                if(board[i][j] == 'O'){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
-                } else  if (board[i][j] == 'T'){
+                } else if (board[i][j] == 'T') {
                     board[i][j] = 'O';
                 }
             }
         }
     }
 
-    public void dfs(char[][] board,int i, int j){
-        if(board == null || board.length == 0 || i < 0 || i >= board.length||
-                j < 0 || j >= board[0].length || board[i][j] != 'O'){
+    public void dfs(char[][] board, int i, int j) {
+        if (board == null || board.length == 0 || i < 0 || i >= board.length ||
+                j < 0 || j >= board[0].length || board[i][j] != 'O') {
             return;
         }
         board[i][j] = 'T';
-        for(int[] d : direct){
-            dfs(board,i + d[0],j + d[1]);
+        for (int[] d : direct) {
+            dfs(board, i + d[0], j + d[1]);
         }
     }
 
+
+    class Solution {
+        public void solve(char[][] board) {
+            if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
+                return;
+            }
+            int row = board.length;
+            int col = board[0].length;
+            for (int i = 0; i < col; i++) {
+                dfs(board, 0, i);
+                dfs(board, row - 1, i);
+            }
+            for (int i = 1; i < row - 1; i++) {
+                dfs(board, i, 0);
+                dfs(board, i, col - 1);
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (board[i][j] == 'T') board[i][j] = 'O';
+                    else board[i][j] = 'X';
+                }
+            }
+        }
+
+        int[][] dire = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        void dfs(char[][] board, int i, int j) {
+            if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1 || board[i][j] != 'O') {
+                return;
+            }
+            board[i][j] = 'T';
+            for (int[] d : dire) {
+                dfs(board, i + d[0], j + d[1]);
+            }
+        }
+    }
 
 }
