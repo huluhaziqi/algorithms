eb3a72a0061f30ec07c6e24fbaa1af9759177ead
huluhaziqi <huluhaziqi@gmail.com>
2020-02-23 16:53:30 +0800

    bfs of leetcode 1091

--- src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/LeetCode1091.java
+++ src/com/huluhaziqi/algorithms/algorithms/bfs_dfs/LeetCode1091.java
@@ -2,19 +2,67 @@ package com.huluhaziqi.algorithms.algorithms.bfs_dfs;
 
 import javafx.util.Pair;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class LeetCode1091 {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[][] grid = {{0}};
-        solution.shortestPathBinaryMatrix(grid);
+//        Solution solution = new Solution();
+//        int[][] grid = {{0}};
+//        solution.shortestPathBinaryMatrix(grid);
+        // [[0,1,0,0,1,1,0],[1,0,0,0,0,0,0],[1,0,0,1,1,1,1],[0,1,0,0,0,0,0],[1,0,0,0,0,0,1],[1,0,0,1,0,0,0],[1,0,1,0,0,1,0]]
+        Solution2 solution2 = new Solution2();
+        int[][] grid = {{0, 1, 0, 0, 1, 1, 0}, {1, 0, 0, 0, 0, 0, 0}, {1, 0, 0, 1, 1, 1, 1}, {0, 1, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0, 1}, {1, 0, 0, 1, 0, 0, 0}, {1, 0, 1, 0, 0, 1, 0}};
+        int result = solution2.shortestPathBinaryMatrix(grid);
+        System.out.println(result);
     }
 
-    static class Solution {
+    class Solution {
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0 ||
+                    grid[0][0] == 1) {
+                return -1;
+            }
+            int row = grid.length;
+            int col = grid[0].length;
+            int[][] direction = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {0, -1}, {1, 0}, {1, -1}, {1, 1}};
+            boolean[][] visited = new boolean[row][col];
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+            int min = Integer.MAX_VALUE;
+            int step = 1;
+            visited[0][0] = true;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size-- > 0) {
+                    int[] top = queue.poll();
+                    if (top[0] == row - 1 && top[1] == col - 1) {
+                        if (step < min) {
+                            min = step;
+                            step = 1;
+                        }
+                    }
+                    for (int[] d : direction) {
+                        int tem1 = top[0] + d[0];
+                        int tem2 = top[1] + d[1];
+                        if (tem1 < 0 || tem1 >= row || tem2 < 0 || tem2 >= col || grid[tem1][tem2] == 1 || visited[tem1][tem2]) {
+                            continue;
+                        }
+                        visited[tem1][tem2] = true;
+                        queue.offer(new int[]{tem1, tem2});
+                    }
+                }
+                step++;
+            }
+            return min == Integer.MAX_VALUE ? -1 : min;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+
+    }
+
+    static class Solution3 {
         public int shortestPathBinaryMatrix(int[][] grid) {
             if (grid == null || grid.length == 0 || grid[0].length == 0 || grid[0][0] == 1) {
                 return -1;
@@ -53,5 +101,57 @@ public class LeetCode1091 {
             return -1;
         }
 
+    }
+
+    /**
+     * 超时
+     */
+    static class Solution2 {
+        public int shortestPathBinaryMatrix(int[][] grid) {
+            if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+                return -1;
+            }
+            int row = grid.length;
+            int col = grid[0].length;
+            boolean[][] visited = new boolean[row][col];
+            if (grid[0][0] == 1) {
+                return -1;
+            }
+            List<Integer> start = new ArrayList<>();
+            start.add(grid[0][0]);
+            visited[0][0] = true;
+            bfs(grid, visited, start, 0, 0, 0, 0, row - 1, col - 1);
+            return min == Integer.MAX_VALUE ? -1 : min;
+        }
+
+        int min = Integer.MAX_VALUE;
+        List<List<Integer>> result = new ArrayList<>();
+
+        void bfs(int[][] grid, boolean[][] visited, List<Integer> midlist, int curx, int cury, int sx, int sy, int ex, int ey) {
+            System.out.println("bfs");
+            if (curx == ex && cury == ey) {
+                result.add(midlist);
+                if (midlist.size() < min) {
+                    min = midlist.size();
+                }
+                System.out.println("success");
+                return;
+            }
+            for (int[] d : direction) {
+                int nx = curx + d[0];
+                int ny = cury + d[1];
+                if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid[0].length || visited[nx][ny] || grid[nx][ny] == 1) {
+                    continue;
+                }
+                visited[curx][cury] = true;
+                midlist.add(grid[curx][cury]);
+                bfs(grid, visited, midlist, nx, ny, sx, sy, ex, ey);
+                visited[curx][cury] = false;
+                midlist.remove(midlist.size() - 1);
+            }
+
+        }
+
+        int[][] direction = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {0, -1}, {1, 0}, {1, -1}, {1, 1}};
     }
 }
