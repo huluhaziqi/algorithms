c948bb96ac4f2a3ed47ef950bb16fc2c8ff0520d
huluhaziqi <huluhaziqi@gmail.com>
2020-02-28 00:32:04 +0800

    [add] leetcode 77

--- src/com/huluhaziqi/algorithms/algorithms/backTracing/Combinations.java
+++ src/com/huluhaziqi/algorithms/algorithms/backTracing/Combinations.java
@@ -5,36 +5,59 @@ import java.util.*;
 public class Combinations {
 
     public static void main(String[] args) {
-            int n = 4;
-            int k = 2;
-        List<List<Integer>> result = new Combinations().combine(n,k);
-        result.forEach(o->{
-            o.forEach(p-> System.out.print(p + " "));
+        int n = 4;
+        int k = 2;
+        List<List<Integer>> result = new Combinations().combine(n, k);
+        result.forEach(o -> {
+            o.forEach(p -> System.out.print(p + " "));
             System.out.print("     ");
         });
     }
 
 
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] visited = new boolean[n + 1];
 
+        backTracing(n, k, 1, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    public void backTracing(int n, int k, int start, List<Integer> value, List<List<Integer>> ret) {
+        if (value.size() == k) {
+            ret.add(new ArrayList<>(value));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            value.add(i);
+            backTracing(n, k, i + 1, value, ret);
+            value.remove(value.size() - 1);
+        }
+    }
+
+    class Solution {
         public List<List<Integer>> combine(int n, int k) {
+            if (k > n) {
+                return new ArrayList<>();
+            }
             List<List<Integer>> result = new ArrayList<>();
-            boolean[] visited = new boolean[n + 1];
-
-            backTracing(n, k,1,new ArrayList<>(), result);
-
+            boolean[] visited = new boolean[n];
+            dfs(n, k, 1, new ArrayList<>(), result);
             return result;
         }
 
-        public void backTracing(int n, int k, int start, List<Integer> value, List<List<Integer>> ret) {
-            if (value.size() == k) {
-                ret.add(new ArrayList<>(value));
+        void dfs(int n, int k, int i, List<Integer> list, List<List<Integer>> result) {
+            if (list.size() == k) {
+                result.add(new ArrayList<>(list));
                 return;
             }
-
-            for (int i = start; i <= n; i++) {
-                value.add(i);
-                backTracing(n, k, i + 1, value, ret);
-                value.remove(value.size() - 1);
+            for (int x = i; x <= n; x++) {
+                list.add(x);
+                dfs(n, k, x + 1, list, result);
+                list.remove(list.size() - 1);
             }
         }
+    }
 }
