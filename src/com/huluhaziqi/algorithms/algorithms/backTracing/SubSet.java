d68f6f86f457a0b840a72c7bcebcaa87535c6961
huluhaziqi <huluhaziqi@gmail.com>
2020-02-29 17:05:50 +0800

    [add] leetcode 78 subsets

--- src/com/huluhaziqi/algorithms/algorithms/backTracing/SubSet.java
+++ src/com/huluhaziqi/algorithms/algorithms/backTracing/SubSet.java
@@ -5,11 +5,47 @@ import java.util.List;
 
 public class SubSet {
 
+    /**
+     * 78. Subsets
+     * Medium
+     * <p>
+     * 2917
+     * <p>
+     * 69
+     * <p>
+     * Add to List
+     * <p>
+     * Share
+     * Given a set of distinct integers, nums, return all possible subsets (the power set).
+     * <p>
+     * Note: The solution set must not contain duplicate subsets.
+     * <p>
+     * Example:
+     * <p>
+     * Input: nums = [1,2,3]
+     * Output:
+     * [
+     * [3],
+     * [1],
+     * [2],
+     * [1,2,3],
+     * [1,3],
+     * [2,3],
+     * [1,2],
+     * []
+     * ]
+     * Accepted
+     * 486,627
+     * Submissions
+     * 839,676
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3,4};
+        int[] nums = {1, 2, 3, 4};
         List<List<Integer>> result = new SubSet().subsets(nums);
-        result.forEach(o->{
-            o.forEach(p-> System.out.print(p + " "));
+        result.forEach(o -> {
+            o.forEach(p -> System.out.print(p + " "));
             System.out.print("     ");
         });
     }
@@ -32,9 +68,24 @@ public class SubSet {
         }
     }
 
-//    public List<List<Integer>> subsets2(int[] nums){
-//        List<List<Integer>> result = new ArrayList<>();
-//        return result;
-//    }
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return new ArrayList<>();
+            }
+            List<List<Integer>> result = new ArrayList<>();
+            dfs(nums, new ArrayList<>(), result, 0);
+            return result;
+        }
+
+        void dfs(int[] nums, List<Integer> list, List<List<Integer>> result, int index) {
+            result.add(new ArrayList<>(list));
+            for (int i = index; i < nums.length; i++) {
+                list.add(nums[i]);
+                dfs(nums, list, result, i + 1);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
 
 }
