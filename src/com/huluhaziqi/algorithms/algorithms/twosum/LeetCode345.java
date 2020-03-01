0dd89d35f67fd3f9ab61f82a3b37e9e69469bfc6
huluhaziqi <huluhaziqi@gmail.com>
2020-03-01 21:23:23 +0800

    [add] leetcode 345 leetcode 633

--- src/com/huluhaziqi/algorithms/algorithms/twosum/LeetCode345.java
+++ src/com/huluhaziqi/algorithms/algorithms/twosum/LeetCode345.java
@@ -2,22 +2,28 @@ package com.huluhaziqi.algorithms.algorithms.twosum;
 
 public class LeetCode345 {
 
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        solution2.reverseVowels("a.b,.");
+    }
+
     class Solution {
         public String reverseVowels(String s) {
-            if(s == null || s.length() == 0){
+            if (s == null || s.length() == 0) {
                 return "";
             }
             char[] chars = s.toCharArray();
             int i = 0;
             int j = chars.length - 1;
-            while(i < j && i < s.length() && j >=0){
-                while(i < s.length() && !isR(chars[i])){
+            while (i < j && i < s.length() && j >= 0) {
+                while (i < s.length() && !isR(chars[i])) {
                     i++;
                 }
-                while(j >= 0 && !isR(chars[j])){
+                while (j >= 0 && !isR(chars[j])) {
                     j--;
                 }
-                if(i <s.length() && j >= 0 &&  i <= j) {
+                if (i < s.length() && j >= 0 && i <= j) {
                     char tmp = chars[i];
                     chars[i] = chars[j];
                     chars[j] = tmp;
@@ -28,9 +34,46 @@ public class LeetCode345 {
             return new String(chars);
         }
 
-        public boolean isR(char b){
+        public boolean isR(char b) {
             char a = Character.toLowerCase(b);
-            if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u'){
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    static class Solution2 {
+        public String reverseVowels(String s) {
+            if (s == null || s.length() <= 1) {
+                return s;
+            }
+            int l = 0;
+            int h = s.length() - 1;
+            char[] chars = s.toCharArray();
+            while (l < h) {
+                while (!isVowels(s.charAt(l))) {
+                    l++;
+                }
+                while (!isVowels(s.charAt(h))) {
+                    h--;
+                }
+                if (l <= h) {
+                    char tem = chars[l];
+                    chars[l] = chars[h];
+                    chars[h] = tem;
+                    l++;
+                    h--;
+                }
+
+            }
+            return String.valueOf(chars);
+        }
+
+
+        boolean isVowels(char c) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                    || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
                 return true;
             }
             return false;
--- src/com/huluhaziqi/algorithms/algorithms/twosum/LeetCode633.java
+++ src/com/huluhaziqi/algorithms/algorithms/twosum/LeetCode633.java
@@ -8,19 +8,28 @@ package com.huluhaziqi.algorithms.algorithms.twosum;
 //        输出: True
 //        解释: 1 * 1 + 2 * 2 = 5
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode633 {
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        solution2.judgeSquareSum(9);
+    }
+
     class Solution {
         public boolean judgeSquareSum(int c) {
-            if(c < 0){
+            if (c < 0) {
                 return false;
             }
             long i = 0;
-            long j = (long) Math.sqrt(c) +1;
-            while(i <= j){
-                long tem = i * i +j * j ;
-                if(tem == c){
+            long j = (long) Math.sqrt(c) + 1;
+            while (i <= j) {
+                long tem = i * i + j * j;
+                if (tem == c) {
                     return true;
-                } else if(tem > c){
+                } else if (tem > c) {
                     j--;
                 } else {
                     i++;
@@ -31,4 +40,46 @@ public class LeetCode633 {
     }
 
 
+    static class Solution2 {
+        public boolean judgeSquareSum(int c) {
+            if (c < 0) {
+                return false;
+            }
+            List<Integer> list = generate(c);
+            for (int i = 0; i < list.size(); i++) {
+                int t = list.get(i);
+                if (t > c) {
+                    return false;
+                }
+                if (binary(list, c - t)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        boolean binary(List<Integer> list, int target) {
+            int i = 0;
+            int j = list.size() - 1;
+            while (i <= j) {
+                int mid = i + (j - i) / 2;
+                if (list.get(mid) == target) {
+                    return true;
+                } else if (list.get(mid) > target) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+            return false;
+        }
+
+        List<Integer> generate(int c) {
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i <= Math.sqrt(c) + 1; i++) {
+                result.add(i * i);
+            }
+            return result;
+        }
+    }
 }
