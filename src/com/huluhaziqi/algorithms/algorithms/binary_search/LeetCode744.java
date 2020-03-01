824862ef8a2175c79801aed2b8f956fca8ddbb57
huluhaziqi <huluhaziqi@gmail.com>
2020-03-01 16:02:47 +0800

    [add] leetcode 540

--- src/com/huluhaziqi/algorithms/algorithms/binary_search/LeetCode744.java
+++ src/com/huluhaziqi/algorithms/algorithms/binary_search/LeetCode744.java
@@ -5,38 +5,38 @@ public class LeetCode744 {
     /**
      * 744. Find Smallest Letter Greater Than Target
      * Easy
-     *
+     * <p>
      * Share
      * Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
-     *
+     * <p>
      * Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
-     *
+     * <p>
      * Examples:
      * Input:
      * letters = ["c", "f", "j"]
      * target = "a"
      * Output: "c"
-     *
+     * <p>
      * Input:
      * letters = ["c", "f", "j"]
      * target = "c"
      * Output: "f"
-     *
+     * <p>
      * Input:
      * letters = ["c", "f", "j"]
      * target = "d"
      * Output: "f"
-     *
+     * <p>
      * Input:
      * letters = ["c", "f", "j"]
      * target = "g"
      * Output: "j"
-     *
+     * <p>
      * Input:
      * letters = ["c", "f", "j"]
      * target = "j"
      * Output: "c"
-     *
+     * <p>
      * Input:
      * letters = ["c", "f", "j"]
      * target = "k"
@@ -48,37 +48,37 @@ public class LeetCode744 {
      */
     class Solution2 {
         public char nextGreatestLetter(char[] letters, char target) {
-            if(letters == null || letters.length == 0){
+            if (letters == null || letters.length == 0) {
                 return ' ';
             }
             int l = 0;
             int r = letters.length - 1;
 
-            while(l <= r){
-                int mid = l +(r - l) / 2;
-                if(letters[mid] <= target){
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (letters[mid] <= target) {
                     l = mid + 1;
                 } else {
                     r = mid - 1;
                 }
             }
-            return l < letters.length ? letters[l] :letters[0];
+            return l < letters.length ? letters[l] : letters[0];
         }
     }
 
     class Solution3 {
         public char nextGreatestLetter(char[] letters, char target) {
-            if(letters == null || letters.length == 0){
+            if (letters == null || letters.length == 0) {
                 return ' ';
             }
             int l = 0;
             int r = letters.length - 1;
-            if(target >= letters[r]){
+            if (target >= letters[r]) {
                 return letters[0];
             }
-            while(l < r){
-                int mid = l +(r - l) / 2;
-                if(letters[mid] <= target){
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (letters[mid] <= target) {
                     l = mid + 1;
                 } else {
                     r = mid;
@@ -86,6 +86,30 @@ public class LeetCode744 {
             }
 
             return letters[l];
+        }
+    }
+
+    /**
+     * 找到左边比目标值大的第一个，
+     * char  <= target , l = mid + 1;
+     * char > target r = mid - 1;
+     */
+    class Solution4 {
+        public char nextGreatestLetter(char[] letters, char target) {
+            if (letters == null || letters.length == 0) {
+                return ' ';
+            }
+            int l = 0;
+            int r = letters.length - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (letters[mid] <= target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            return l >= letters.length ? letters[0] : letters[l];
         }
     }
 }
--- src/com/huluhaziqi/algorithms/algorithms/binary_search/SingleElement.java
+++ src/com/huluhaziqi/algorithms/algorithms/binary_search/SingleElement.java
@@ -3,8 +3,11 @@ package com.huluhaziqi.algorithms.algorithms.binary_search;
 public class SingleElement {
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 2, 4, 4, 5, 5,9};
-        int result = new SingleElement().singleNonDuplicate(nums);
+//        int[] nums = {1, 1, 2, 2, 4, 4, 5, 5, 9};
+//        int result = new SingleElement().singleNonDuplicate(nums);
+//        System.out.println(nums[result]);
+        int[] nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+        int result = new Solution().singleNonDuplicate(nums);
         System.out.println(nums[result]);
     }
 
@@ -17,7 +20,7 @@ public class SingleElement {
         int r = len - 1;
         while (l < r) {
             int mid = l + (r - l) / 2;
-            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return mid;
             }
             if ((mid % 2 == 1 && nums[mid] == nums[mid + 1]) ||
@@ -28,5 +31,73 @@ public class SingleElement {
             }
         }
         return l;
+    }
+
+    static class Solution {
+        public int singleNonDuplicate(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int l = 0;
+            int r = nums.length - 1;
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                int len1 = mid - l;
+                if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                    return nums[mid];
+                }
+                if (len1 % 2 == 0) {
+                    if (nums[mid] == nums[mid - 1]) {
+                        r = mid;
+                    } else if (nums[mid] == nums[mid + 1]) {
+                        l = mid;
+                    }
+                }
+                if (len1 % 2 == 1) {
+                    if (nums[mid] == nums[mid + 1]) {
+                        r = mid - 1;
+                    } else if (nums[mid] == nums[mid - 1]) {
+                        l = mid + 1;
+                    }
+                }
+
+            }
+            return nums[l];
+
+        }
+    }
+
+    /**
+     * 全部异或
+     */
+    class Solution2 {
+        public int singleNonDuplicate(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int first = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                first ^= nums[i];
+            }
+            return first;
+        }
+    }
+
+    class Solution3 {
+        public int singleNonDuplicate(int[] nums) {
+            int l = 0, h = nums.length - 1;
+            while (l < h) {
+                int m = l + (h - l) / 2;
+                if (m % 2 == 1) {
+                    m--;   // 保证 l/h/m 都在偶数位，使得查找区间大小一直都是奇数
+                }
+                if (nums[m] == nums[m + 1]) {
+                    l = m + 2;
+                } else {
+                    h = m;
+                }
+            }
+            return nums[l];
+        }
     }
 }
