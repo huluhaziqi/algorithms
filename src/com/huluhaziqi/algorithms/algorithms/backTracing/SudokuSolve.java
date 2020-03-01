292e29b8dd03d4a40014b066fec1c8caca38f946
huluhaziqi <huluhaziqi@gmail.com>
2020-03-01 13:37:35 +0800

    [add] leetcode 37

--- src/com/huluhaziqi/algorithms/algorithms/backTracing/SudokuSolve.java
+++ src/com/huluhaziqi/algorithms/algorithms/backTracing/SudokuSolve.java
@@ -3,6 +3,44 @@ package com.huluhaziqi.algorithms.algorithms.backTracing;
 public class SudokuSolve {
 
 
+    /**
+     * 37. Sudoku Solver
+     * Hard
+     * <p>
+     * 1390
+     * <p>
+     * 82
+     * <p>
+     * Add to List
+     * <p>
+     * Share
+     * Write a program to solve a Sudoku puzzle by filling the empty cells.
+     * <p>
+     * A sudoku solution must satisfy all of the following rules:
+     * <p>
+     * Each of the digits 1-9 must occur exactly once in each row.
+     * Each of the digits 1-9 must occur exactly once in each column.
+     * Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+     * Empty cells are indicated by the character '.'.
+     * <p>
+     * <p>
+     * A sudoku puzzle...
+     * <p>
+     * <p>
+     * ...and its solution numbers marked in red.
+     * <p>
+     * Note:
+     * <p>
+     * The given board contain only digits 1-9 and the character '.'.
+     * You may assume that the given Sudoku puzzle will have a single unique solution.
+     * The given board size is always 9x9.
+     * Accepted
+     * 165,128
+     * Submissions
+     * 403,184
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
                 {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
@@ -50,7 +88,6 @@ public class SudokuSolve {
     }
 
 
-
     private boolean backtracking(char[][] board, int row, int col) {
         if (col >= 9) {
             return backtracking(board, row + 1, 0);
@@ -90,5 +127,66 @@ public class SudokuSolve {
         return true;
     }
 
+    class Solution {
+        private char[][] board;
+
+        public void solveSudoku(char[][] board) {
+            this.board = board;
+            if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) {
+                return;
+            }
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[i][j] == '.') {
+                        continue;
+                    }
+                    int c = board[i][j] - '0';
+                    rowUsed[i][c] = true;
+                    colUsed[j][c] = true;
+                    cubeUsed[cubeNum(i, j)][c] = true;
+                }
+            }
+            dfs(0, 0);
+        }
+
+        boolean dfs(int row, int col) {
+
+            while (row < 9 && board[row][col] != '.') {
+                row = col == 8 ? row + 1 : row;
+                col = col == 8 ? 0 : col + 1;
+            }
+            if (row == 9) {
+                return true;
+            }
+            for (int k = 1; k <= 9; k++) {
+
+                if (rowUsed[row][k] || colUsed[col][k] || cubeUsed[cubeNum(row, col)][k]) {
+                    continue;
+                }
+                rowUsed[row][k] = colUsed[col][k] = cubeUsed[cubeNum(row, col)][k] = true;
+                char c = (char) (k + '0');
+                board[row][col] = c;
+                if (dfs(row, col)) {
+                    return true;
+                }
+                board[row][col] = '.';
+                rowUsed[row][k] = colUsed[col][k] = cubeUsed[cubeNum(row, col)][k] = false;
+            }
+            return false;
+
+
+        }
+
+        int cubeNum(int i, int j) {
+            int x = i / 3;
+            int y = j / 3;
+            return x * 3 + y;
+        }
+
+        boolean rowUsed[][] = new boolean[9][10];
+        boolean colUsed[][] = new boolean[9][10];
+        boolean cubeUsed[][] = new boolean[9][10];
+
+    }
 
 }
