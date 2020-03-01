58577800a9a21e5bcb76ea953c1f67b9da6772d9
huluhaziqi <huluhaziqi@gmail.com>
2020-03-01 17:28:21 +0800

    [add] leetcode 34 of search range

--- src/com/huluhaziqi/algorithms/algorithms/binary_search/SearchForARange.java
+++ src/com/huluhaziqi/algorithms/algorithms/binary_search/SearchForARange.java
@@ -4,12 +4,46 @@ import java.util.Arrays;
 
 public class SearchForARange {
 
+    /**
+     * leetcode 34
+     * 34. Find First and Last Position of Element in Sorted Array
+     * Medium
+     * <p>
+     * 2631
+     * <p>
+     * 117
+     * <p>
+     * Add to List
+     * <p>
+     * Share
+     * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+     * <p>
+     * Your algorithm's runtime complexity must be in the order of O(log n).
+     * <p>
+     * If the target is not found in the array, return [-1, -1].
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [5,7,7,8,8,10], target = 8
+     * Output: [3,4]
+     * Example 2:
+     * <p>
+     * Input: nums = [5,7,7,8,8,10], target = 6
+     * Output: [-1,-1]
+     * Accepted
+     * 424,426
+     * Submissions
+     * 1,209,532
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] nums = {2,2};
+        int[] nums = {2, 2};
         int target = 2;
-        int index = new SearchForARange().maxIndex(nums,target);
+        int index = new SearchForARange().maxIndex(nums, target);
         System.out.println(index);
     }
+
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[2];
         Arrays.fill(result, -1);
@@ -21,7 +55,7 @@ public class SearchForARange {
         System.out.println("l " + l + " r " + r);
         if (l >= 0 && l < nums.length - 1 && nums[l] == target) {
             result[0] = l;
-            while(r < nums.length && nums[r] == target){
+            while (r < nums.length && nums[r] == target) {
                 r++;
             }
             result[1] = r;
@@ -58,4 +92,32 @@ public class SearchForARange {
 
     }
 
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return new int[]{-1, -1};
+            }
+            int start = binary_search(nums, target);
+            int end = binary_search(nums, target + 1) - 1;
+            if (start == nums.length || nums[start] != target) {
+                return new int[]{-1, -1};
+            } else {
+                return new int[]{start, end};
+            }
+        }
+
+        int binary_search(int nums[], int target) {
+            int l = 0;
+            int h = nums.length;
+            while (l < h) {
+                int mid = l + (h - l) / 2;
+                if (nums[mid] >= target) {
+                    h = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return l;
+        }
+    }
 }
