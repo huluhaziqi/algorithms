f478b2697024bc6156d6c526ed5867a6b89e7389
huluhaziqi <huluhaziqi@gmail.com>
2020-03-01 22:51:30 +0800

    [add] leetcode 112 path sum of tree

--- src/com/huluhaziqi/algorithms/algorithms/tree/LeetCode112.java
+++ src/com/huluhaziqi/algorithms/algorithms/tree/LeetCode112.java
@@ -96,4 +96,18 @@ public class LeetCode112 {
             bfs(current.right, sum, value + current.val, list);
         }
     }
+
+    class Solution2 {
+        public boolean hasPathSum(TreeNode root, int sum) {
+            if (root == null) {
+                return false;
+            }
+            if (root.right == null && root.left == null && sum == root.val) {
+                return true;
+            }
+            return hasPathSum(root.left, sum - root.val)
+                    || hasPathSum(root.right, sum - root.val);
+
+        }
+    }
 }
