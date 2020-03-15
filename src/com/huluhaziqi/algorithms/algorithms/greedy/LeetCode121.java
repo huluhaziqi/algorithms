7669161da0a27d5f00d72d5826ff6de09d0719a7
huluhaziqi <huluhaziqi@gmail.com>
2020-03-15 16:14:06 +0800

    [add] leetcode 121 605

--- src/com/huluhaziqi/algorithms/algorithms/greedy/LeetCode121.java
+++ src/com/huluhaziqi/algorithms/algorithms/greedy/LeetCode121.java
@@ -5,34 +5,34 @@ public class LeetCode121 {
     /**
      * 121. Best Time to Buy and Sell Stock
      * Easy
-     *
+     * <p>
      * Share
      * Say you have an array for which the ith element is the price of a given stock on day i.
-     *
+     * <p>
      * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
-     *
+     * <p>
      * Note that you cannot sell a stock before you buy one.
-     *
+     * <p>
      * Example 1:
-     *
+     * <p>
      * Input: [7,1,5,3,6,4]
      * Output: 5
      * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-     *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+     * Not 7-1 = 6, as selling price needs to be larger than buying price.
      * Example 2:
-     *
+     * <p>
      * Input: [7,6,4,3,1]
      * Output: 0
      * Explanation: In this case, no transaction is done, i.e. max profit = 0.
      */
     class Solution {
         public int maxProfit(int[] prices) {
-            if(prices == null || prices.length < 2){
+            if (prices == null || prices.length < 2) {
                 return 0;
             }
             int max = 0;
             int tem = 0;
-            for(int i = 0; i < prices.length - 1; i++){
+            for (int i = 0; i < prices.length - 1; i++) {
                 tem += prices[i + 1] - prices[i];
                 tem = Math.max(tem, prices[i + 1] - prices[i]);
                 max = Math.max(max, tem);
@@ -40,20 +40,21 @@ public class LeetCode121 {
             return max;
         }
     }
+
     class Solution2 {
         public int maxProfit(int[] prices) {
-            if(prices == null || prices.length < 2){
+            if (prices == null || prices.length < 2) {
                 return 0;
             }
             int[] gap = new int[prices.length - 1];
-            for(int i = 0; i < prices.length - 1; i++){
+            for (int i = 0; i < prices.length - 1; i++) {
                 gap[i] = prices[i + 1] - prices[i];
             }
             int max = 0;
             int start = 0;
             int tem = 0;
-            while(start < gap.length) {
-                if(gap[start] + tem <= 0){
+            while (start < gap.length) {
+                if (gap[start] + tem <= 0) {
                     tem = 0;
                 } else {
                     tem += gap[start];
@@ -62,6 +63,48 @@ public class LeetCode121 {
                 start++;
             }
             return max;
+        }
+    }
+
+    class Solution3 {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length < 2) {
+                return 0;
+            }
+            int[] dp = new int[prices.length - 1];
+            for (int i = 1; i < prices.length; i++) {
+                dp[i - 1] = prices[i] - prices[i - 1];
+            }
+            int global = 0;
+            int local = 0;
+            for (int i = 0; i < dp.length; i++) {
+                if (dp[i] + local < 0) {
+                    local = 0;
+                } else {
+                    local += dp[i];
+                    global = Math.max(global, local);
+                }
+            }
+            return global;
+        }
+    }
+
+    class Solution4 {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length < 2) {
+                return 0;
+            }
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < prices.length; i++) {
+                if (prices[i] < min) {
+                    min = prices[i];
+                } else if (prices[i] - min > max) {
+                    max = prices[i] - min;
+                }
+            }
+            return max;
+
         }
     }
 }
--- src/com/huluhaziqi/algorithms/algorithms/greedy/LeetCode605.java
+++ src/com/huluhaziqi/algorithms/algorithms/greedy/LeetCode605.java
@@ -5,12 +5,12 @@ public class LeetCode605 {
     /**
      * 605. Can Place Flowers
      * Easy
-     *
+     * <p>
      * Share
      * Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
-     *
+     * <p>
      * Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
-     *
+     * <p>
      * Example 1:
      * Input: flowerbed = [1,0,0,0,1], n = 1
      * Output: True
@@ -39,6 +39,31 @@ public class LeetCode605 {
                 if (count >= n) {
                     return true;
                 }
+            }
+            return false;
+        }
+    }
+
+    class Solution2 {
+        public boolean canPlaceFlowers(int[] flowerbed, int n) {
+            if (flowerbed == null || flowerbed.length == 0 || flowerbed.length < n) {
+                return false;
+            }
+            int count = 0;
+            for (int i = 0; i < flowerbed.length; i++) {
+                int pre = 0;
+                int next = 0;
+                if (flowerbed[i] == 0) {
+                    pre = (i == 0) ? 0 : flowerbed[i - 1];
+                    next = (flowerbed.length - 1 == i) ? 0 : flowerbed[i + 1];
+                    if (pre == 0 && next == 0) {
+                        count++;
+                        flowerbed[i] = 1;
+                    }
+                }
+            }
+            if (count >= n) {
+                return true;
             }
             return false;
         }
