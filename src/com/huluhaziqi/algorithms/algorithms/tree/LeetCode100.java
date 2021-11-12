26a026c3a9d6ac77174808543702173877c3f29e
huluhaziqi <huluhaziqi@gmail.com>
2021-11-12 15:26:32 +0800

    leetcode 100 judge two tree are the same

---
+++ src/com/huluhaziqi/algorithms/algorithms/tree/LeetCode100.java
@@ -0,0 +1,16 @@
+package com.huluhaziqi.algorithms.algorithms.tree;
+
+public class LeetCode100 {
+
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p == null && q == null){
+                return true;
+            }
+            if(p == null ||q == null){
+                return false;
+            }
+            return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+    }
+}
