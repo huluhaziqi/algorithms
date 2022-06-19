5d4aa3086df70c1d5619c2c6569f3387cf572496
huluhaziqi <huluhaziqi@gmail.com>
2022-06-19 16:56:05 +0800

    leetcode 677 sum of trie

--- src/com/huluhaziqi/algorithms/algorithms/tree/LeetCode677.java
+++ src/com/huluhaziqi/algorithms/algorithms/tree/LeetCode677.java
@@ -269,6 +269,83 @@ public class LeetCode677 {
 
     }
 
+    class MapSum5 {
+        private class Node {
+            Node[] childs = new Node[26];
+            boolean isLeaf = false;
+            int val = 0;
+        }
+
+        private Node root = new Node();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        public MapSum5() {
+
+        }
+
+        public void insert(String key, int val) {
+            map.put(key, val);
+            insert(key, root, val);
+        }
+
+        private void insert(String key, Node node, int val) {
+            if (node == null) {
+                return;
+            }
+            if (key.length() == 0) {
+                node.isLeaf = true;
+                node.val = val;
+                return;
+            }
+            int v = key.charAt(0) - 'a';
+            if (node.childs[v] == null) {
+                node.childs[v] = new Node();
+            }
+            insert(key.substring(1), node.childs[v], val);
+        }
+
+        public int sum(String prefix) {
+            return sum(prefix, root);
+        }
+
+        public int sum(String prefix, Node node) {
+            if (node == null) {
+                return 0;
+            }
+            for (int i = 0; i < prefix.length(); i++) {
+                int index = prefix.charAt(i) - 'a';
+                if (node.childs[index] == null) {
+                    return 0;
+                }
+                node = node.childs[index];
+            }
+            return dfs(node);
+        }
+
+        int dfs(Node node) {
+            if (node == null) {
+                return 0;
+            }
+            int sum = 0;
+            if (node.val > 0) {
+                sum += node.val;
+            }
+            for (Node child : node.childs) {
+                sum += dfs(child);
+            }
+            return sum;
+        }
+
+    }
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
+
 /**
  * Your MapSum object will be instantiated and called as such:
  * MapSum obj = new MapSum();
